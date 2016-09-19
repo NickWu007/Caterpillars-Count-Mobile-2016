9d265161226d7ef587b01e8f1981f25423426a60
palmour <palmour@live.unc.edu>
2016-09-18 20:35:09 -0400

    Hide/show pending surveys.

--- www/js/homepage.js
+++ www/js/homepage.js
@@ -16,6 +16,20 @@ $(document).ready(function(){
         
     } 
     $(".survey-count").html("Total Stored Survey: " + $list_length); //updates survey-count
+    $(".pending-button").click(function(){
+        $(".background").toggle();
+        var $pending_button_content = $(".pending-button").text();
+        var elem = document.getElementById("filler");
+        if($pending_button_content==="Show Pending Surveys"){
+            $(".pending-button").html("Hide Pending Surveys");
+            elem.setAttribute("style", "height:0px");
+        }
+        else{
+            $(".pending-button").html("Show Pending Surveys");
+            elem.setAttribute("style", "height:200px");
+        }
+    }); 
+    //hides and shows pending surveys
 });
 //Handles device rotation
 window.shouldRotateToOrientation = function() {
