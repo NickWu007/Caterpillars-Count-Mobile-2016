164d1db1709ddd0fb655d8b145af2a883cb125e5
Christopher Palmour <palmour@live.unc.edu>
2016-09-26 19:40:41 -0400

    pending.js looks for databse

--- www/js/pending.js
+++ www/js/pending.js
@@ -3,6 +3,55 @@
 */
 $(document).ready(function(){
 
+    document.addEventListener("deviceready", onDeviceReady, false);
     var $list_length = $(".survey_item").length; //computes number of items in the survey list
-    $(".survey-count").html("Total Stored Survey: " + $list_length); //updates survey-count
+        $(".survey-count").html("Total Stored Survey: " + $list_length); //updates survey-count
+
+    function  closeDB(){
+        db.close(function(){
+            alert("DB closed");
+        }, function(error){
+            alert("DB Closing Error:"+error.message);
+        });
+    }
+    
+    function onDeviceReady() {
+       document.addEventListener("backbutton", function (e) {
+            e.preventDefault();
+            closeDB();
+            navigator.app.exitApp();
+        }, false);
+
+        window.sqlitePlugin.echoTest(function(){
+            console.log("echo test ok");
+        });
+
+        window.sqlitePlugin.selfTest(function(){
+            console.log("self test ok");
+        });
+
+        function DBsuccess(){
+            alert("DB open ok, Create Table etc");
+        }
+
+        var db = window.sqlitePlugin.openDatabase({name: 'app.db', location: 'default'},
+            DBsuccess(),
+            function(error){
+                alert("Error Open Database:"+JSON.stringify(error));
+            }
+        );
+
+        var first_time=true;
+        db.transaction(function(tx){
+            tx.executeSQL('select distinct site, circle, survey, time, id from SURVEY', [], function(tx, rs){
+
+            });
+        }, function(errror){
+            alert("Transaction Error" + error.message);
+        }, function(){
+            alert("Successfully retrieved pending surveys");
+        }
+        );
+    }
+
 });
