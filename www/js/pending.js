ee4d43bb541128621022582669a914fbbe598b76
Christopher Palmour <palmour@live.unc.edu>
2016-09-27 11:20:56 -0400

    b/c

--- www/js/pending.js
+++ www/js/pending.js
@@ -1,9 +1,13 @@
 /**
  * Created by palmour on 9/20/16
 */
+var db;
+var DOMAIN = "http://develop-caterpillars.vipapps.unc.edu";
+document.addEventListener("deviceready", onDeviceReady, false);
+
 $(document).ready(function(){
 
-    document.addEventListener("deviceready", onDeviceReady, false);
+    
     var $list_length = $(".survey_item").length; //computes number of items in the survey list
         $(".survey-count").html("Total Stored Survey: " + $list_length); //updates survey-count
 
@@ -14,57 +18,56 @@ $(document).ready(function(){
             alert("DB Closing Error:"+error.message);
         });
     }
+});
     
-    function onDeviceReady() {
-       document.addEventListener("backbutton", function (e) {
-            e.preventDefault();
-            closeDB();
-            navigator.app.exitApp();
-        }, false);
+function onDeviceReady() {
+    document.addEventListener("backbutton", function (e) {
+        e.preventDefault();
+        closeDB();
+        navigator.app.exitApp();
+    }, false);
 
-        window.sqlitePlugin.echoTest(function(){
-            console.log("echo test ok");
-        });
+    window.sqlitePlugin.echoTest(function(){
+        console.log("echo test ok");
+    });
 
-        window.sqlitePlugin.selfTest(function(){
-            console.log("self test ok");
-        });
+    window.sqlitePlugin.selfTest(function(){
+        console.log("self test ok");
+    });
 
-        function DBsuccess(){
-            alert("DB open ok, Create Table etc");
-        }
-
-        var resultset;
-        var db = window.sqlitePlugin.openDatabase({name: 'app.db', location: 'default'},
-            DBsuccess(),
-            function(error){
-                alert("Error Open Database:"+JSON.stringify(error));
-            }
-        );
-
-        var first_time=true;
-        db.transaction(function(tx){
-            tx.executeSQL('select distinct site, circle, survey, time, id from SURVEY', [], function(tx, rs){
-                resultset = rs.rows;
-                alert("Rows retrieved: "+resultset.length);
-            });
-        }, function(errror){
-            alert("Transaction Error" + error.message);
-        }, function(){
-            alert("Successfully retrieved pending surveys");
-
-            var new_list_item;
-
-            for(var i=0; i<resultset.length; i++){
-                var row = resultset.item(i);
-
-                new_list_item = "<li class=\"survey_item\"><h5>Site: "+row.site+"</h5><h5>Circle: " +
-                    row.circle+"</h5><h5>Survey: "+row.survey+"</h5><h5>Time: "+row.time+"</h5><h5>id: "+
-                    row.id+"</h5>";
-                $(".survey_list ul").append(new_list_item);
-            }
-        }
-        );
+    function DBsuccess(){
+        alert("DB open ok, Create Table etc");
     }
 
-});
+    var resultset;
+    var db = window.sqlitePlugin.openDatabase({name: 'app.db', location: 'default'},
+        DBsuccess(),
+        function(error){
+            alert("Error Open Database:"+JSON.stringify(error));
+        }
+    );
+        
+}
+
+var retrievePendingSurveys = function(){
+    $.ajax({
+        url : DOMAIN + "/api/surveys.php", //name probably not correct. verify when testing.
+        type: "POST",
+        crossDomain: true,
+        dataType: 'json',
+        data: JSON.stringify({
+            "action" :"getAllSiteState" //parameters probably incorrect. verify.
+        }), 
+        success: function(survey_result){
+            populateSurveyList(site_result);
+        }, 
+        error : function(){
+            navigator.notification.alert("Unexpected error retrieving pending surveys.");
+        }
+    });
+};
+
+var populateSurveyList = function(survey_result){
+
+};
+
