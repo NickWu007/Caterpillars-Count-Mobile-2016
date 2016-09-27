c68c464b24103cbc96158d45d39262e73b75426a
Christopher Palmour <palmour@live.unc.edu>
2016-09-26 20:15:39 -0400

    Append new list items to survey list.

--- www/js/pending.js
+++ www/js/pending.js
@@ -34,6 +34,7 @@ $(document).ready(function(){
             alert("DB open ok, Create Table etc");
         }
 
+        var resultset;
         var db = window.sqlitePlugin.openDatabase({name: 'app.db', location: 'default'},
             DBsuccess(),
             function(error){
@@ -44,12 +45,24 @@ $(document).ready(function(){
         var first_time=true;
         db.transaction(function(tx){
             tx.executeSQL('select distinct site, circle, survey, time, id from SURVEY', [], function(tx, rs){
-
+                resultset = rs.rows;
+                alert("Rows retrieved: "+resultset.length);
             });
         }, function(errror){
             alert("Transaction Error" + error.message);
         }, function(){
             alert("Successfully retrieved pending surveys");
+
+            var new_list_item;
+
+            for(var i=0; i<resultset.length; i++){
+                var row = resultset.item(i);
+
+                new_list_item = "<li class=\"survey_item\"><h5>Site: "+row.site+"</h5><h5>Circle: " +
+                    row.circle+"</h5><h5>Survey: "+row.survey+"</h5><h5>Time: "+row.time+"</h5><h5>id: "+
+                    row.id+"</h5>";
+                $(".survey_list ul").append(new_list_item);
+            }
         }
         );
     }
