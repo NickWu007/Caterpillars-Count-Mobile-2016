a7855984ba342402fc95b2418730b8b6229b67e3
matwu1006 <mingjunw@live.unc.edu>
2016-09-27 23:08:22 -0400

    db_offline_store

--- www/js/start-screen.js
+++ www/js/start-screen.js
@@ -44,6 +44,7 @@ $(document).ready(function() {
         var firsttime=true;
         db.transaction(function(tx){
             tx.executeSql("CREATE TABLE IF NOT EXISTS USER_INFO (name, password, userId)");
+            tx.executeSql("CREATE TABLE IF NOT EXISTS SURVEY(type, siteID, userID, password, circle, survey, timeStart, temperatureMin, temperatureMax, siteNotes, plantSpecies, herbivory, surveyType, leafCount, source)");
             tx.executeSql('SELECT count(*) AS NUM from USER_INFO',[], function(tx, rs){
                 alert("#lines in db:"+rs.rows.item(0).NUM);
                 if(parseInt(rs.rows.item(0).NUM)>0){
